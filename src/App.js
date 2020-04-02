a8c64d8ba1e8392d77c6da669732cab56afef5c9
elantsev <elantsev.pa@gmail.com>
2020-04-02 10:30:24 +0300

    .

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App () {
         scale={[140, 150, 160, 170]}
         data1={{ date: '31.12.16', value: 150 }}
         data2={{ date: '30.09.17', value: 165.6 }} /> */}
-      <SandClocksSVG shift={0}
+      <SandClocksSVG shift={0} className="item"
         scale={[140, 150, 160, 170]}
         data1={{ date: '31.12.16', value: 140 }}
         data2={{ date: '30.09.17', value: 170 }} />
@@ -31,9 +31,9 @@ function App () {
         data1={{ date: '31.12.16', value: 160.6 }}
         data2={{ date: '30.09.17', value: 142.1 }} />
 
-      <SandClockSVG value={0.65} height="100" width="60" />
+      {/* <SandClockSVG value={0.65} height="100" width="60" />
       <PieChart startDegree={90} value={0.1} color={'url(#gradient)'} />
-      <SpeedometerChart value={0.85} />
+      <SpeedometerChart value={0.85} /> */}
     </div>
   );
 }
